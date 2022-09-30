dc7da30dbc8ff29b0edf52fc4541b4664dde7b9e
aditya <adityakumar222333@gmail.com>
2022-09-30 20:10:10 +0530

    🚧 Work in Progress:  saving the users Saving the users and when they do, adding their corresponding orders

--- db.js
+++ db.js
@@ -36,3 +36,65 @@ const user3 = new User({
   order: [],
 });
 
+// Saving the users, I know that there are better ways of doing this but since we're not getting more data I find it ok.
+user1
+  .save()
+  .then(async () => {
+    await user1.setOrder(Order, {
+      orderID: 880,
+      amount: 379,
+      date: "2020-07-25T14:10:26.113Z",
+    });
+    await user1.setOrder(Order, {
+      orderID: 881,
+      amount: 479,
+      date: "2020-08-25T14:10:26.113Z",
+    });
+    await user1.setOrder(Order, {
+      orderID: 882,
+      amount: 579,
+      date: "2020-09-25T14:10:26.113Z",
+    });
+  })
+  .catch((e) => console.warn(e));
+user2
+  .save()
+  .then(async () => {
+    await user2.setOrder(Order, {
+      orderID: 883,
+      amount: 379,
+      date: "2020-07-25T14:10:26.113Z",
+    });
+    await user2.setOrder(Order, {
+      orderID: 884,
+      amount: 479,
+      date: "2020-08-25T14:10:26.113Z",
+    });
+  })
+  .catch((e) => console.warn(e));
+user3
+  .save()
+  .then(async () => {
+    await user3.setOrder(Order, {
+      orderID: 886,
+      amount: 379,
+      date: "2020-07-25T14:10:26.113Z",
+    });
+    await user3.setOrder(Order, {
+      orderID: 887,
+      amount: 479,
+      date: "2020-08-25T14:10:26.113Z",
+    });
+    await user3.setOrder(Order, {
+      orderID: 888,
+      amount: 579,
+      date: "2020-09-25T14:10:26.113Z",
+    });
+    await user3.setOrder(Order, {
+      orderID: 889,
+      amount: 579,
+      date: "2020-10-25T14:10:26.113Z",
+    });
+  })
+  .catch((e) => console.warn(e));
+
